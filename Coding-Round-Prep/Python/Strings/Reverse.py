cbb6487a64b3459ded3596563012b9cce5b86b63
Surendradev333 <gsrgsreddy3@gmail.com>
2026-06-07 16:16:57 +0530

    Added Reversal String

---
+++ Coding-Round-Prep/Python/Strings/Reverse.py
@@ -0,0 +1,21 @@
+# Input String
+s = "hello"
+
+# Reverse the string using slicing
+reversed_string = s[::-1] # start-end-step
+
+# Print the reversed string
+print(reversed_string)
+
+
+# Method 2: without slicing
+s= "HI"
+reversed_string = ""
+for char in s:
+    reversed_string = char + reversed_string
+print(reversed_string)
+
+# Method 3: using built-in function
+s = "hello"
+reversed_string = ''.join(reversed(s))
+print(reversed_string)
