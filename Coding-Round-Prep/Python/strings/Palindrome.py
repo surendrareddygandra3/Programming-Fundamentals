8d2647af4e6a870ca9ab6f2ac51c252bd4a6302c
Surendradev333 <gsrgsreddy3@gmail.com>
2026-06-07 16:31:57 +0530

    Added Reversal String

---
+++ Coding-Round-Prep/Python/strings/Palindrome.py
@@ -0,0 +1,22 @@
+# Input String 
+s = "madam"
+
+# Check if the string is equal to its reverse
+if s == s[::-1]:
+    print(f"{s} is a palindrome.")
+else:
+    print(f"{s} is not a palindrome.")
+
+
+# Method 2: without slicing
+s = "madam"
+
+reversed_string = ""
+
+for char in s:
+    reversed_string = char + reversed_string
+
+if s == reversed_string:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
