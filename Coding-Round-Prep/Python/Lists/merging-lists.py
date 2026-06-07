9aacd1bc9cb97098e86120cbb9cf25a8acc9860a
Surendradev333 <gsrgsreddy3@gmail.com>
2026-06-07 17:23:10 +0530

    Added Lists: Merging Lists

---
+++ Coding-Round-Prep/Python/Lists/merging-lists.py
@@ -0,0 +1,25 @@
+# First list
+list1 = [1,2,3]
+
+# Second list
+list2 = [4,5,6]
+
+# Merge lists
+result = list1 + list2
+
+# Print result
+print(result)
+
+
+
+# Method 2: using extend()
+# First list
+list1 = [1,2,3]
+
+# Second list
+list2 = [4,5,6]
+
+# Add all elements of list2 into list1
+list1.extend(list2)
+
+print(list1)
