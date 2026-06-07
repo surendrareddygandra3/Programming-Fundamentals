380e165c7caaf09217ef383aa3ac394e162ece0d
Surendradev333 <gsrgsreddy3@gmail.com>
2026-06-07 18:28:25 +0530

    Added OOPS: Polymorphism

---
+++ Coding-Round-Prep/Python/OOPS/Polymorphism.py
@@ -0,0 +1,25 @@
+# Dog class
+class Dog:
+
+    def sound(self):
+
+        print("Bark")
+
+
+# Cat class
+class Cat:
+
+    def sound(self):
+
+        print("Meow")
+
+
+# Objects
+d = Dog()
+
+c = Cat()
+
+# Same method
+d.sound()
+
+c.sound()
