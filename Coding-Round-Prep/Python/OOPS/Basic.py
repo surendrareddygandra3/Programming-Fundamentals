ad6d9acb48e62179cbb090f08b99fca50f7583e1
Surendradev333 <gsrgsreddy3@gmail.com>
2026-06-07 18:01:21 +0530

    Added OOPS: Basics

---
+++ Coding-Round-Prep/Python/OOPS/Basic.py
@@ -0,0 +1,30 @@
+# Create class
+class Student:
+
+    pass
+
+
+# Create object
+s1 = Student()
+
+# Add attributes
+s1.name = "Surendra"
+s1.age = 23
+
+# Print values
+print(s1.name)
+print(s1.age)
+
+
+# Method 2: 
+
+class Student:
+
+    name = "Surendra"
+    age = 23
+
+
+s1 = Student()
+
+print(s1.name)
+print(s1.age)
